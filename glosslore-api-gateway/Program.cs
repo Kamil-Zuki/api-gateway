5f2c77ecd29ee14a9119db9c6ad592daa5b58934
Kamil <karatov7@gmail.com>
2022-11-07 15:31:41 +0300

    The project was created

---
+++ glosslore-api-gateway/Program.cs
@@ -0,0 +1,15 @@
+namespace glosslore_api_gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
